292e94acf12ad795176da0d4dc6e86525171ddb3
Michael Mattson <michael.p.mattson@gmail.com>
2021-04-23 08:58:41 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+famous_person = 'Beyonce'
+is_active = true
+
+famous_person = 'LeBron James'
+
+num_3 = 7
+num_9 = 22
+
+sum = num_3 + num_9
+
+animals = ['penguin', 'shark', 'zebra']
+
+animals.shift
+
+p famous_person.length
