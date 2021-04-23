3766f8c907c0a1dc89ba8b6f794ebdee94762245
Michael Mattson <michael.p.mattson@gmail.com>
2021-04-23 09:23:41 -0600

    Add hash models

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  :name => 'Park Elementary',
+  :city => 'Tuskalooga',
+  :student_num => 216,
+  :avg_test_score => 82.2,
+  :in_session => true,
+  :full_classrooms => false,
+  :first_grade_teachers => ["Mr Jones", "Ms Crabapple", "Mr Rosenthal"],
+  :class_pets => ['Tommy Turtle', 'Spiderman', 'Chicken Little']
+}
---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  species: 'cat',
+  name: 'Mr Business',
+  age: 9,
+  weight_in_lbs: 14,
+  likes_other_animals: false,
+  has_chip: true,
+  favorite_foods: ['fancy feast', "mom's dinner", 'bugs'],
+  favorite_scratching_places: ['carpet', 'couch', 'bathroom door']
+}
